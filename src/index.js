f8ea863f7965317d8ff4473c965297ce5f5091a3
leonardom249 <leonardo.megan@gmail.com>
2018-04-16 12:45:37 -0500

    added components to redux, everything working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {Provider} from 'react-redux';
+import {Provider} from 'react-redux';
 
 
 import store from './store';
-//To Manually Test we must import actions using destructuting (take function vals not capitalized vals)
+//Note to Self: To Manually Test we must import actions using destructuting (take function vals not capitalized vals)
 //and do store.dispatch with a specific action and associated values(if any needed)
 //(ex: '5' for guess in the ADD_GUESS action):
-import {addGuess, restartGame, generateAuralUpdate} from './actions';
 
 import './reset.css';
 import './index.css';
@@ -15,21 +14,25 @@ import './index.css';
 import Game from './components/game';
 
 ReactDOM.render(
-    <Game />,
+  <Provider store={store}>
+    <Game />
+    </Provider>,
   document.getElementById('root')
 );
 
-store.dispatch(addGuess(5));
-console.log(store.getState());
 
-store.dispatch(addGuess(12));
-console.log(store.getState());
+// testing pre-adding of components to redux:
+// store.dispatch(addGuess(5));
+// console.log(store.getState());
 
-store.dispatch(restartGame());
-console.log(store.getState());
+// store.dispatch(addGuess(12));
+// console.log(store.getState());
 
-store.dispatch(addGuess(25));
-console.log(store.getState());
+// store.dispatch(restartGame());
+// console.log(store.getState());
 
-store.dispatch(generateAuralUpdate());
-console.log(store.getState());
+// store.dispatch(addGuess(25));
+// console.log(store.getState());
+
+// store.dispatch(generateAuralUpdate());
+// console.log(store.getState());
